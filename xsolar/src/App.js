8f5875b3f992c896f3be575c48f39baf46c5fef1
vinniekun <vinniekun@gmail.com>
2020-01-19 23:11:46 -0200

    react init

---
+++ xsolar/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// *** React ***
+// Componente
+// Estado
+// Propriedade
+
+function App() {
+  return (
+    <h1>Hello World</h1>
+  );
+}
+
+export default App;
---
+++ xsolar/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+//JSX Javascript + XML
+//App ./App.js renderiza no root
+ReactDOM.render(<App />, document.getElementById('root'));
+
